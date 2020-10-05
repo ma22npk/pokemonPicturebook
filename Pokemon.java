d54b95e9843ac35d8f5fd7b7657e242f377d4c94
Kota Matsumoto <k.matsumoto@blue-corporation.jp>
2020-10-06 03:37:10 +0900

    no message

--- Pokemon.java
+++ Pokemon.java
@@ -5,14 +5,13 @@ class Pokemon {
   // 名前を入れる変数を定義
   public String name;
 
-  public void hello() {
-    System.out.println("なまえ" + this.name);
-  }
-
   // コンストラクタの定義
   Pokemon() {
     System.out.println("インスタンスが生成されました");
+  }
 
+  public void hello() {
+    System.out.println("なまえ" + this.name);
   }
 
 }
