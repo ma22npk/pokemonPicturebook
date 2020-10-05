eda7191226a2835c8b230e4f3b5b46fe26663d50
Kota Matsumoto <k.matsumoto@blue-corporation.jp>
2020-10-06 03:24:28 +0900

    インスタンスの生成

---
+++ Main.java
@@ -0,0 +1,17 @@
+class Main {
+  public static void main(String[] args) {
+    // フシギダネ
+    Pokemon fushigidane = new Pokemon();
+    fushigidane.name = "フシギダネ";
+    fushigidane.hello();
+    // ヒトカゲ
+    Pokemon hitokage = new Pokemon();
+    hitokage.name = "ヒトカゲ";
+    hitokage.hello();
+    // ゼニガメ
+    Pokemon zenigame = new Pokemon();
+    zenigame.name = "ゼニガメ";
+    zenigame.hello();
+  }
+
+}
---
+++ Pokemon.java
@@ -0,0 +1,12 @@
+/**
+ * Pokemon
+ */
+class Pokemon {
+  // 名前を入れる変数を定義
+  public String name;
+
+  public void hello() {
+    System.out.println("なまえ" + this.name);
+  }
+
+}
