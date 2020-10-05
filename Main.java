3b59c98a277b7c85284fea207e15d04832d05cb3
Kota Matsumoto <k.matsumoto@blue-corporation.jp>
2020-10-06 04:20:05 +0900

    フィールドとコンストラクタの定義

--- Main.java
+++ Main.java
@@ -1,17 +1,14 @@
 class Main {
   public static void main(String[] args) {
-    // フシギダネ
-    Pokemon fushigidane = new Pokemon();
-    fushigidane.name = "フシギダネ";
-    fushigidane.hello();
-    // ヒトカゲ
-    Pokemon hitokage = new Pokemon();
-    hitokage.name = "ヒトカゲ";
-    hitokage.hello();
-    // ゼニガメ
-    Pokemon zenigame = new Pokemon();
-    zenigame.name = "ゼニガメ";
-    zenigame.hello();
+    Pokemon pokemon1 = new Pokemon("フシギダネ", "たねポケモン", "くさ", 0.7, 6.9,
+        "大きな種を背負っているくさタイプのポケモン。この種は生まれた時から背中に植わっており、生後しばらくは種から養分を得て成長する。");
+    System.out.println(pokemon1.name);
+    System.out.println(pokemon1.category);
+    System.out.println(pokemon1.type);
+    System.out.println(pokemon1.height);
+    System.out.println(pokemon1.weight);
+    System.out.println(pokemon1.description);
+
   }
 
 }
--- Pokemon.java
+++ Pokemon.java
@@ -4,14 +4,28 @@
 class Pokemon {
   // 名前を入れる変数を定義
   public String name;
+  // 分類 を入れる変数を定義
+  public String category;
+  // タイプ を入れる変数を定義
+  public String type;
+  // たかさ を入れる変数を定義
+  public double height;
+  // おもさ を入れる変数を定義
+  public double weight;
+  // 説明文 を入れる変数を定義
+  public String description;
 
   // コンストラクタの定義
-  Pokemon() {
-    System.out.println("インスタンスが生成されました");
+  Pokemon(String name, String category, String type, Double height, Double weight, String description) {
+    this.name = name;
+    this.category = category;
+    this.type = type;
+    this.height = height;
+    this.weight = weight;
+    this.description = description;
   }
 
   public void hello() {
-    System.out.println("なまえ" + this.name);
+    System.out.println("なまえ:" + this.name);
   }
-
 }
